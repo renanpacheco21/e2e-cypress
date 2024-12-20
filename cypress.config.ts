f088d1b38e244e8a67bebef2bfde804832930d8d
@renan.matos <renanpacheco.delta@gmail.com>
2024-12-20 17:44:02 -0300

    Commit ajustes da config

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,8 +3,8 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   viewportWidth: 1280,
   viewportHeight: 880,
-  defaultCommandTimeout: 30000,
-  pageLoadTimeout: 60000,
+  //defaultCommandTimeout: 5000,
+  //pageLoadTimeout: 30000,
   videoCompression: false,
   chromeWebSecurity: false,
   retries: 0,
